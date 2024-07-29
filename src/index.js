a7595d7cfbfc0da4bc20d56f8248e06e47e233a5
INNA-KUSHNIRR1 <149315820+INNA-KUSHNIRR1@users.noreply.github.com>
2024-07-30 00:58:20 +0200

    add  connect

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
 
 // const message = 'Hello world';
 // console.log(message);
 
 setupServer();
+initMongoConnection();
