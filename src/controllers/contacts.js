5bc5d777cf8bc8752f724ea76aecf25388f98234
INNA-KUSHNIRR1 <149315820+INNA-KUSHNIRR1@users.noreply.github.com>
2024-08-04 21:35:50 +0200

    add status201

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -34,7 +34,7 @@ export const getContactByIdController = async (req, res, next) => {
 
 export const createContactController = async (req, res) => {
   const contact = await createContact(req.body);
-  res.json({
+  res.status(201).json({
     status: 201,
     message: 'Successfully created a contact',
     data: contact,
