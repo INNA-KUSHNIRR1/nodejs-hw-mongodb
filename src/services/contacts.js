330bf6d7ad2b19b93d5225664ebf833eeceaf375
INNA-KUSHNIRR1 <149315820+INNA-KUSHNIRR1@users.noreply.github.com>
2024-08-09 21:56:08 +0200

    add filter

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -7,11 +7,19 @@ export const getAllContacts = async ({
   perPage = 10,
   sortOrder = SORT_ORDER.ASC,
   sortBy = '_id',
+  filter = {},
 }) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
   const contactsQuery = ContactsCollection.find();
+
+  if (filter.contactType) {
+    contactsQuery.where('contactType').equals(filter.contactType);
+  }
+  if (typeof filter.isFavourite !== 'undefined') {
+    contactsQuery.where('isFavourite').equals(filter.isFavourite);
+  }
   const contactsCount = await ContactsCollection.find()
     .merge(contactsQuery)
     .countDocuments();
---
+++ src/utils/parseFilterParams.js
@@ -0,0 +1,29 @@
+const parseType = (type) => {
+  const isString = typeof type === 'string';
+  if (!isString) return;
+
+  const isType = (type) => ['work', 'home', 'personal'].includes(type);
+
+  if (isType(type)) return type;
+};
+
+const parseBoolean = (boolean) => {
+  const isString = typeof boolean === 'string';
+  if (!isString) return undefined;
+  if (boolean.toLowerCase() === 'true') return true;
+  if (boolean.toLowerCase() === 'false') return false;
+
+  return undefined;
+};
+
+export const parseFilterParams = (query) => {
+  const { contactType, isFavourite } = query;
+
+  const parsedType = parseType(contactType);
+  const parsedIsFavourite = parseBoolean(isFavourite);
+
+  return {
+    contactType: parsedType,
+    isFavourite: parsedIsFavourite,
+  };
+};
