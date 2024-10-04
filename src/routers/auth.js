3e09a2e1bad4ba8532dba9137ac780e845e7a65f
INNA-KUSHNIRR1 <149315820+INNA-KUSHNIRR1@users.noreply.github.com>
2024-10-04 16:19:47 +0200

    add userId

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -13,7 +13,7 @@ const router=Router();
 
 router.post("/register", validateBody(registerUserSchema), ctrlWrapper(registerUserController),);
 router.post("/login", validateBody(loginUserSchema), ctrlWrapper(loginUserController),);
-router.post("/logout", ctrlWrapper(logoutUserController));
 router.post("/refresh", ctrlWrapper(refreshUserSessionController));
+router.post("/logout", ctrlWrapper(logoutUserController));
 
 export default router;
