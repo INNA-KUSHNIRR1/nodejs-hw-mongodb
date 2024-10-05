43d0f60907d63841198e67c2b9bd9c7f34619f31
INNA-KUSHNIRR1 <149315820+INNA-KUSHNIRR1@users.noreply.github.com>
2024-10-05 12:21:31 +0200

    fixed isValidId

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -81,6 +81,7 @@ export const deleteContactController = async (req, res, next) => {
   const userId = req.user._id;
 
   const contact = await deleteContact(contactId, userId);
+
   if (!contact) {
     next(createHttpError(404, 'Contact not found'));
     return;
