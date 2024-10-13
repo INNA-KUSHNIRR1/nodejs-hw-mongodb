e585912b40835e7edd98e86f54e0c1722f198e7c
INNA-KUSHNIRR1 <149315820+INNA-KUSHNIRR1@users.noreply.github.com>
2024-10-13 12:56:42 +0200

    add post contact with photo

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -55,7 +55,19 @@ export const getContactByIdController = async (req, res, next) => {
 export const createContactController = async (req, res) => {
   const userId=req.user._id;
  const {body}=req;
-  const contact = await createContact(body, userId);
+ const photo = req.file;
+
+ let photoUrl;
+
+ if(photo){
+   if(env("ENABLE_CLOUDINARY")==="true"){
+     photoUrl = await saveFileToCloudinary(photo);
+   }else{
+     photoUrl = await saveFileToUploadDir(photo);
+   }
+
+ }
+  const contact = await createContact({...body, photo: photoUrl,}, userId);
 
   res.status(201).json({
     status: 201,
